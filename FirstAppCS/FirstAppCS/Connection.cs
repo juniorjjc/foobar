ec07c813e33c0e3a3efcf8db6fabb0f5eb4a34b1
juniorjjc <juanchotacorta.ctm@gmail.com>
2016-07-06 20:02:53 -0600

    DB connection implemented

---
+++ FirstAppCS/FirstAppCS/Connection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace FirstAppCS
+{
+    class Connection
+    {
+
+        public Connection() {
+            this.server = "localhost";
+            this.database = "expediente";
+            this.uid = "root";
+            this.password = "root";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            this.connection = new MySqlConnection(connectionString);         
+        }
+    
+        public MySqlConnection getConnection() {
+            return connection;
+        }
+
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        
+        
+    }
+
+    
+}
